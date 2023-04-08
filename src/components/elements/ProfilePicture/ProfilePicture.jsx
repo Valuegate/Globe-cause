16a3c48fecc93572630242b555ef4f2a5561dc51
Lezzy <koladeelizabeth01@gmail.com>
2023-04-08 13:29:17 +0100

    feat: community

--- src/components/elements/ProfilePicture/ProfilePicture.jsx
+++ src/components/elements/ProfilePicture/ProfilePicture.jsx
@@ -1,16 +1,35 @@
 import styles from "./styles.module.css";
 import {useState} from 'react'
-import imge from '../../../assets/myPhoto.JPG'
+import imge from '../../../assets/Group 11966.png'
+import { useUserAuth } from "../../../hooks/auth/UserAuthContext";
+
+import { db } from "../../../firebase";
+import {  doc, setDoc } from "firebase/firestore";
 
 const ProfilePicture = ({  alt, placeholder }) => {
 
 const [src, setImage] = useState([imge,])
+
+const {  user } = useUserAuth();
 
 const onImageChange = (event) => {
  if (event.target.files && event.target.files[0]) {
    setImage(URL.createObjectURL(event.target.files[0]));
  }
 }
+
+ const createProfile = async (id) => {
+    await onImageChange()
+    setDoc(doc(db, "volunteers", id), {
+      profile_image_url: src,
+    })
+      .then(() => {
+        console.log("Document successfully written!");
+      })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      });
+  };
   return (
       <form runat="server">
         <label for="inputTag" style={{cursor:'pointer'}} >
