4b63c65c0c7d08574f5d004e758e31185b76a584
semight <davidoluwasemiloorei@gmail.com>
2024-01-13 22:06:34 +0100

    commenting some codes out

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,44 +1,44 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
 
-import styles from "./styles.module.css";
+// import styles from "./styles.module.css";
 
-import Intro from "../../components/elements/Intro/Intro";
-import Search from "../../components/elements/Search/Search";
-import Cities from "../../components/containers/Cities/Cities";
-import BottomNavigation from "../../components/containers/BottomNavigation/BottomNavigation";
+// import Intro from "../../components/elements/Intro/Intro";
+// import Search from "../../components/elements/Search/Search";
+// import Cities from "../../components/containers/Cities/Cities";
+// import BottomNavigation from "../../components/containers/BottomNavigation/BottomNavigation";
 
-import NavigationButton from "../../components/elements/NavigationButton/NavigationButton";
-import Patners from "../../components/containers/Patners/Patners";
-import Nav from "./Nav";
+// import NavigationButton from "../../components/elements/NavigationButton/NavigationButton";
+// import Patners from "../../components/containers/Patners/Patners";
+// import Nav from "./Nav";
 
 
-const Home = () => {
-  const [navigationState, setNavigationState] = useState(0);
-  const [filter, setFilter] = useState("");
+// const Home = () => {
+//   const [navigationState, setNavigationState] = useState(0);
+//   const [filter, setFilter] = useState("");
 
-  const bottomNavigation = () => {
-    setNavigationState(!navigationState);
-  };
+//   const bottomNavigation = () => {
+//     setNavigationState(!navigationState);
+//   };
 
-  return (
-    <div
-      className={styles.Home}
+//   return (
+//     <div
+//       className={styles.Home}
       
-    >
-      <Nav />
-      <Intro />
-      <Search setFilter={setFilter} />
-      <Patners />
-      <Cities filter={filter} />
-      <BottomNavigation navigationState={navigationState} />
-      <NavigationButton
-        onClick={bottomNavigation}
-        navigationState={navigationState}
-      />
+//     >
+//       <Nav />
+//       <Intro />
+//       <Search setFilter={setFilter} />
+//       <Patners />
+//       <Cities filter={filter} />
+//       <BottomNavigation navigationState={navigationState} />
+//       <NavigationButton
+//         onClick={bottomNavigation}
+//         navigationState={navigationState}
+//       />
       
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default Home;
+// export default Home;
